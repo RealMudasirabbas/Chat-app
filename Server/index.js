6080c413b957a1a895e97fd36304fc0db83f3230
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2023-10-17 22:15:48 +0500

    Please add them

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,18 @@
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 5500 });
+import { createServer } from "http"
+import { Server } from "socket.io"
+const httpServer = createServer()
+const io = new Server (httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"]
+    }
+}) 
 
-server.on('connection', socket => {
-    socket.on('message', message => {
-        const b = Buffer.from(message);
-        console.log(b.toString());
-        socket.send(`${message}`);
+io.on('connection', socket => {
+    console.log(`User: ${socket.id} connected`)
+    socket.on('message', data => {
+        console.log(data)
+        io.emit('message',`${socket.id.substring(0,5)}${data}`)
     });
 });
+
+httpServer.listen(3500, () => console.log('listening on port 3500'))
