eff77db610515aceb8664e78b4600c110a342579
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2023-10-18 21:21:52 +0500

    Please add them

--- App/app.js
+++ App/app.js
@@ -1,18 +1,38 @@
-const socket = new WebSocket('ws://localhost:5500')
+// const socket = io ('ws://localhost:3500')
+
+// function sendMessage(e) {
+//     e.preventDefault();
+//     const input = document.querySelector('input');
+//     if(input.value){
+//         socket.emit(input.value)
+//         input.value = ''
+//     }
+//     input.focus()
+// }
+
+// document.querySelector('form').addEventListener('submit', sendMessage);
+// socket.on('message', ( data ) => {
+//     const li = document.createElement('li');
+//     li.textContent = data
+//     document.querySelector('ul').appendChild(li)
+// })
+
+const socket = io('ws://localhost:3500');
 
 function sendMessage(e) {
     e.preventDefault();
     const input = document.querySelector('input');
-    if(input.value){
-        socket.send(input.value)
-        input.value = ''
+    if (input.value) {
+        socket.emit('message', input.value);
+        input.value = '';
     }
-    input.focus()
+    input.focus();
 }
 
 document.querySelector('form').addEventListener('submit', sendMessage);
-socket.addEventListener('message', ( {data} ) => {
+
+socket.on('message', (data) => {
     const li = document.createElement('li');
-    li.textContent = data
-    document.querySelector('ul').appendChild(li)
-})
+    li.textContent = data;
+    document.querySelector('ul').appendChild(li);
+});
