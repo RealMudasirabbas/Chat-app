4a5a1bd9cb7ed18101ffa721288d34ce629a275f
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2023-10-19 16:46:42 +0500

    Please add them

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,17 @@
-// 
-import { createServer } from "http";
+import express from 'express';
 import { Server } from "socket.io";
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500
+const app = express()
+app.use(express.static(path.join(__dirname,"public")))
+const expressServer = app.listen(PORT,() => {
+    console.log(`listening on ${PORT}`)
+})
+const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
@@ -18,4 +26,3 @@ io.on('connection', socket => {
     });
 });
 
-httpServer.listen(3500, () => console.log('listening on port 3500'));
